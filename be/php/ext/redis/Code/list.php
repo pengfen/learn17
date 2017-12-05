285a36f82682c00cd38e1ba573c5278cf9c5b117
ricky <ricky@leoedu.com>
2017-12-05 09:47:35 +0800

    f

--- be/php/ext/redis/Code/list.php
+++ be/php/ext/redis/Code/list.php
@@ -14,3 +14,15 @@ $redis->rpush('list', 'tail1');
 $redis->rpush('list', 'tail2');
 
 echo '列表长度是 : '.$redis->llen('test');
+
+$redis = Redis::connection('cache_nick'); // 连接数据库
+// 向数据库中添加元素
+var_dump($redis -> lPush('favorite_fruit','cherry'));
+var_dump($redis -> lPush('favorite_fruit','banana'));
+
+// 测试数据库中取出数据
+$len = $redis -> lLen('favorite_fruit'); // 获取长度
+for ($i = 1; $i <= $len; $i ++) {
+    var_dump($redis -> rPop('favorite_fruit')); // 获取元素并移除
+}
+var_dump($redis -> lLen('favorite_fiuit')); // 如果长度为0 则退出
