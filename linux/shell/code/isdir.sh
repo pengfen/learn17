7e557f355025400d37f6c9cbe38a0ebc146840ab
ricky <ricky@leoedu.com>
2017-12-06 16:31:14 +0800

    f

--- linux/shell/code/isdir.sh
+++ linux/shell/code/isdir.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#read
+#-t 等待时间(秒数) -t 30 等待输入时间30秒
+#-p 提示信息
 read -t 30 -p "Please input a dir : " dir
 if [ -d "$dir" ]
 then
@@ -7,4 +10,12 @@ then
 else
     echo "you are wrong"
 fi
+
+#localhost:~/peng/test/shell$ chmod +x isdir.sh 
+#localhost:~/peng/test/shell$ ./isdir.sh 
+#请输入一个目录: isdir.sh
+#你输入的不是目录
+#localhost:~/peng/test/shell$ ./isdir.sh 
+#请输入一个目录: /var
+#你输入的是目录
 
